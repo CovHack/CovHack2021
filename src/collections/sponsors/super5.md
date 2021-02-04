4c5a61c36b81e012628f3e66f71b6609643f957c
OneNormalUsername <42769194+OneNormalUsername@users.noreply.github.com>
2021-02-04 18:17:25 +0000

    Add Super5 as a sponsor for CovHack 2021
    `heightOverride` was needed to ensure consistency with other sponsors logo

---
+++ src/collections/sponsors/super5.md
@@ -0,0 +1,8 @@
+---
+name: Super 5
+url: 
+logo: /raimbow-sprimkles/super5-logo.png
+tier: starter
+type: sponsor
+heightOverride: 100px
+---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -239,6 +239,7 @@ export const pageQuery = graphql`
             logo
             tier
             type
+            heightOverride
           }
         }
       }
